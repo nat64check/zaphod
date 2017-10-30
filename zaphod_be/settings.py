09434e0fca51da4d738855347fe2b71fdca3e141
Sander Steffann <sander@steffann.nl>
2017-10-30 11:06:26 +0100

    Clean up a bit, make PyCharm happy

--- zaphod_be/settings.py
+++ zaphod_be/settings.py
@@ -12,18 +12,16 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
 # SECURITY WARNING: keep the secret key used in production secret!
 from .local_settings import DEBUG, SECRET_KEY
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = []
 
 # Application definition
+# noinspection PyUnresolvedReferences
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
