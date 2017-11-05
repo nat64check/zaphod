ed4ba5a87fcde112294a36de8cf1433e682f69c5
Sander Steffann <sander@steffann.nl>
2017-11-05 22:23:48 +0100

    Clean up the settings a bit

--- zaphod_be/settings.py
+++ zaphod_be/settings.py
@@ -110,21 +110,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # noinspection PyUnresolvedReferences
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
 
 RUNSERVERPLUS_SERVER_ADDRESS_PORT = '[::]:8000'
