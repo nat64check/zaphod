d76e62370bfa8deba409a183607137735cff4250
Sander Steffann <sander@steffann.nl>
2017-11-05 17:41:43 +0100

    Make instance admin a bit prettier

--- instances/admin.py
+++ instances/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.gis.admin.options import OSMGeoAdmin
+from django.forms.models import ModelForm
 from django.utils.translation import gettext_lazy as _
+from django_countries.widgets import CountrySelectWidget
 
 from instances.filters import TrillianRegionFilter
 from instances.models import Trillian
@@ -10,11 +12,27 @@ class SearchableGeoAdmin(OSMGeoAdmin):
     map_template = 'searchable_osm.html'
 
 
+class TrillianAdminForm(ModelForm):
+    class Meta:
+        model = Trillian
+        widgets = {
+            'country': CountrySelectWidget(),
+        }
+        fields = '__all__'
+
+
 @admin.register(Trillian)
 class TrillianAdmin(SearchableGeoAdmin):
-    list_display = ('name', 'hostname', 'version', 'country', 'admin_full_name')
+    form = TrillianAdminForm
+    list_display = ('name', 'hostname', 'version', 'admin_country', 'admin_full_name')
     list_filter = (TrillianRegionFilter,)
     ordering = ('name',)
+
+    def admin_country(self, trillian):
+        return '{} {}'.format(trillian.country.unicode_flag, trillian.country.name)
+
+    admin_country.short_description = _('country')
+    admin_country.admin_order_field = 'country'
 
     def admin_full_name(self, trillian):
         return trillian.admin.get_full_name()
